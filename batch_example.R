f7bc384aaef25e40bae9d12a178b2be0ab95230e
Pier Lorenzo Paracchini <pparacch@hotmail.com>
2017-09-19 14:29:41 +0200

    Reviewing content of sections - WIP

---
+++ batch_example.R
@@ -0,0 +1,10 @@
+# Move in the folder where R.exe is installed
+# D:
+# cd D:\APPL\R\R-3.3.3\bin
+
+# Run the script in BATCH mode
+# R CMD BATCH e:\APPL\workspace_r\R_programming_101\batch_example.R e:\APPL\workspace_r\R_programming_101\batch_output.txt
+
+print("I am running in batch mode ....")
+print("Here some information about the session...")
+sessionInfo()
